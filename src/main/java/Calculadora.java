e72eb49cccf5c26d38fb90d8ae40732b3d3edaf8
Lucas <130715175+Lucascorado007@users.noreply.github.com>
2024-05-23 23:33:02 +0000

    Calculadora 1.0

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,43 +1,87 @@
 import java.util.Scanner;
 
 class Calculadora{
-  private int numero1;
-  private int numero2;
-  private int total;
+  private float numero1;
+  private float numero2;
+  private float total;
 
   Scanner ler = new Scanner(System.in);
+  
+
+  
+  
   public void soma(){
     Scanner n1 = new Scanner(System.in);
     Calculadora calc = new Calculadora();
     
-    int total;
+    float total;
     System.out.println("calculadora de soma\n\nInsira o 1° numero: ");
-    calc.setNumero1(n1.nextInt());
+    calc.setNumero1(n1.nextFloat());
     System.out.println("Insira o 2° numero: ");
-    calc.setNumero2(n1.nextInt());
+    calc.setNumero2(n1.nextFloat());
     total = calc.getNumero1() + calc.getNumero2();
     System.out.println("\nO resultado é: "+total);
     
   }
 
+  public void mult(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    float total;
+    System.out.println("calculadora de multiplicação\n\nInsira o 1° numero: ");
+    calc.setNumero1(n1.nextFloat());
+    System.out.println("Insira o 2° numero: ");
+    calc.setNumero2(n1.nextFloat());
+    total = calc.getNumero1() * calc.getNumero2();
+    System.out.println("\nO resultado é: "+total);
+
+  }
+
 
   
+  public void sub(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    float total;
+    System.out.println("calculadora de subtração\n\nInsira o 1° numero: ");
+    calc.setNumero1(n1.nextFloat());
+    System.out.println("Insira o 2° numero: ");
+    calc.setNumero2(n1.nextFloat());
+    total = calc.getNumero1() - calc.getNumero2();
+    System.out.println("\nO resultado é: "+total);
+  }
+  public void div(){
+    Scanner n1 = new Scanner(System.in);
+    Calculadora calc = new Calculadora();
+
+    float total;
+    System.out.println("calculadora de divisão\n\nInsira o 1° numero: ");
+    calc.setNumero1(n1.nextFloat());
+    System.out.println("Insira o 2° numero: ");
+    calc.setNumero2(n1.nextFloat());
+    total = calc.getNumero1() / calc.getNumero2();
+    System.out.println("\nO resultado é: "+total);
+  }
+ 
+  
   
   
 
-  public int getNumero1(){
+  public Float getNumero1(){
     return numero1;
   }
-  public void setNumero1(int numero1){
+  public void setNumero1(float numero1){
     this.numero1 = numero1;
   }
-  public int getNumero2(){
+  public float getNumero2(){
     return numero2;
   }
-  public void setNumero2(int numero2){
+  public void setNumero2(float numero2){
     this.numero2 = numero2;
   }
-  public int getTotal(){
+  public float getTotal(){
     return total;
   }
   
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,57 @@ import java.util.Scanner;
 
 class Main{
   public static void main(String[] args){
- 
+    Scanner ler = new Scanner(System.in);
     Calculadora calc = new Calculadora();
-    calc.soma();
+    int choince;
+    do {
+    menu();
+    System.out.println("Escolha uma opção: ");
+    choince = ler.nextInt();  
+    
+    
+    switch(choince){
+      case 1:
+        calc.soma();
+        break;
+      case 2:
+        calc.sub();
+        break;
+      case 3:
+        calc.mult();
+        break;
+      case 4:
+        calc.div();
+        break;
+      case 5:
+        System.out.println("Encerrando programa:");
+        break;
+
+      default:
+        System.out.println("Opção invalida");
+        break;
+    }
+    }
+    while(choince != 5);
+    
+    
+    
+      
     
   }
+    
+  
+  public static void menu(){
+      System.out.println("-----MENU-----");
+      System.out.println("1 - soma");
+      System.out.println("2 - subtração");
+      System.out.println("3 - multiplicação");
+      System.out.println("4 - divisão");
+      System.out.println("5 - sair");
+    }
 }
+ 
+
+
+
+
